50e192a1c1c9a20054a4787d79ca2e43d63c5382
ManuGMoya <manugmoya@gmail.com>
2018-03-31 18:31:54 +0200

    Merge branch 'alertdialog_ventanasregistro' of https://github.com/AnnaPS/BAA into login_mail_pass
    # Conflicts:
    #	BandsnArts/.idea/caches/build_file_checksums.ser
    #	BandsnArts/app/google-services.json

--- BandsnArts/app/src/main/java/com/example/pc/bandsnarts/RegistrarGrupo.java
+++ BandsnArts/app/src/main/java/com/example/pc/bandsnarts/RegistrarGrupo.java
@@ -12,7 +12,7 @@ public class RegistrarGrupo extends AppCompatActivity {
     Spinner spinnerEstilos;
 
 
-    EditText edtMailGrupo,edtPassGrupo,edtRepitePassGrupo;
+    EditText edtMailGrupo,edtPassGrupo,edtRepitePassGrupo,edtNombreGrupo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,22 +20,21 @@ public class RegistrarGrupo extends AppCompatActivity {
         setContentView(R.layout.activity_registrar_grupo);
 
         spinnerEstilos=findViewById(R.id.spEstiloVRegGrupo);
-        spinnerEstilos.setAdapter(new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,getResources().getStringArray(R.array.estiloMusical)));
-
+        spinnerEstilos.setAdapter(new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item
+                ,getResources().getStringArray(R.array.estiloMusical)));
 
         //Nos traemos las vistas
         edtMailGrupo=findViewById(R.id.edtEmailVRegGrupo);
         edtPassGrupo=findViewById(R.id.edtPassVRegGrupo);
         edtRepitePassGrupo=findViewById(R.id.edtRepetirPassVRegGrupo);
-
+        edtNombreGrupo=findViewById(R.id.edtNombreVRegGrupo);
     }
 
 
     public void clickRegGrupo(View view) {
-        if(edtMailGrupo.getText().toString().isEmpty()||edtPassGrupo.getText().toString().isEmpty()||edtRepitePassGrupo.getText().toString().isEmpty()){
+        if(edtMailGrupo.getText().toString().isEmpty()||edtPassGrupo.getText().toString().isEmpty()||edtRepitePassGrupo.getText().toString().isEmpty()||edtNombreGrupo.getText().toString().isEmpty()){
             Toast.makeText(this, "DEBE COMPLETAR TODOS LOS CAMPOS", Toast.LENGTH_SHORT).show();
         }else{
-
             // DEFINIR MAXIMO DE CARACTERES EN CADA CAMPO EN LA PARTE DE DISEÑO!!!!!!!!!!
 
             // Comprobamos que el patron de correo y de contraseña son correctos
