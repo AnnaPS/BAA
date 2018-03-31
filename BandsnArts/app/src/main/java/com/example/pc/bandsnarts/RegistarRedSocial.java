498a55619e17f9ad75ec32ea1fca8ff37e10add7
AnnaPS <anna-91_@hotmail.com>
2018-03-31 17:50:31 +0200

    #terminado layout registro red social

---
+++ BandsnArts/app/src/main/java/com/example/pc/bandsnarts/RegistarRedSocial.java
@@ -0,0 +1,23 @@
+package com.example.pc.bandsnarts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class RegistarRedSocial extends AppCompatActivity {
+    Spinner spinnerInstrumentos,spinnerEstilos;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //finds///
+        setContentView(R.layout.activity_registar_red_social);
+        spinnerEstilos=findViewById(R.id.spEstiloVRegSocial);
+
+        //spinners para estilo musical e instrumentos
+        spinnerInstrumentos=findViewById(R.id.spInstrumentoVRegSocial);
+        spinnerEstilos.setAdapter(new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,getResources().getStringArray(R.array.estiloMusical)));
+        spinnerInstrumentos.setAdapter(new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,getResources().getStringArray(R.array.instrumentos)));
+    }
+}
