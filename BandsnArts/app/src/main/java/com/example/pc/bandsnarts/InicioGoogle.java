9d07d45baf4b9ce46bfc5126aa8668a5f0fe7207
IvanSanchezArranz <ivansanchezarranz@gmail.com>
2018-04-02 13:19:06 +0200

    Merge branch 'login_google' of https://github.com/AnnaPS/BAA into estructura_BD
    # Conflicts:
    #	BandsnArts/.idea/caches/build_file_checksums.ser
    #	BandsnArts/app/src/main/AndroidManifest.xml

--- BandsnArts/app/src/main/java/com/example/pc/bandsnarts/InicioGoogle.java
+++ BandsnArts/app/src/main/java/com/example/pc/bandsnarts/InicioGoogle.java
@@ -1,18 +1,17 @@
 package com.example.pc.bandsnarts;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
