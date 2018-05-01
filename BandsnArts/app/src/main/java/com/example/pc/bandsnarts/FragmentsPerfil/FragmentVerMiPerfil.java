09e4826e5e08c180f36b13b92c2454cc438be7ee
Jaouad93 <elabouyijaouad@gmail.com>
2018-05-01 02:33:04 +0200

    #implemetado boton para probar reproducir un audio en el fragment Multimedia, restringidos algunos tamaños como maxLenght en las clases de login y registro.

--- BandsnArts/app/src/main/java/com/example/pc/bandsnarts/FragmentsPerfil/FragmentVerMiPerfil.java
+++ BandsnArts/app/src/main/java/com/example/pc/bandsnarts/FragmentsPerfil/FragmentVerMiPerfil.java
@@ -16,8 +16,6 @@ import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -31,7 +29,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.FrameLayout;
@@ -52,10 +49,9 @@ import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 import static android.app.Activity.RESULT_OK;
-import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 import static com.facebook.FacebookSdk.getApplicationContext;
 
 
