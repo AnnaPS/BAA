d0b27c08fa66852f9c219a102f9408046368e51e
AnnaPS <anna-91_@hotmail.com>
2018-06-02 18:34:27 +0200

    Merge branch 'optimizacion_arreglos' of https://github.com/AnnaPS/BAA into creacion_contactos
    # Conflicts:
    #	BandsnArts/.idea/caches/build_file_checksums.ser
    #	BandsnArts/app/src/main/res/layout/activity_login.xml

--- BandsnArts/app/src/main/java/com/example/pc/bandsnarts/FragmentsMenuDrawer/FragmentConfiguracion.java
+++ BandsnArts/app/src/main/java/com/example/pc/bandsnarts/FragmentsMenuDrawer/FragmentConfiguracion.java
@@ -7,19 +7,36 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.pc.bandsnarts.Activities.VentanaInicialApp;
 import com.example.pc.bandsnarts.R;
 
 
 public class FragmentConfiguracion extends Fragment {
+    private Button cambiarPass, dejarRecordarPass, eliminarCuenta;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View vista =inflater.inflate(R.layout.fragment_fragment_configuracion, container, false);
+        cambiarPass=vista.findViewById(R.id.btnCambiarPassConfiguracion);
+        dejarRecordarPass=vista.findViewById(R.id.btnDejarDeRecordarPassConfiguracion);
+        eliminarCuenta=vista.findViewById(R.id.btnEliminarCuentaConfiguracion);
 
-        return inflater.inflate(R.layout.fragment_fragment_configuracion, container, false);
+
+
+        cambiarPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentDialogCambiarContraseña cambiarPass= new FragmentDialogCambiarContraseña();
+                android.app.FragmentManager fm = getActivity().getFragmentManager();
+                cambiarPass.setCancelable(false);
+                cambiarPass.show(fm, "AlertaDescartar");
+            }
+        });
+        return vista;
     }
 
 
